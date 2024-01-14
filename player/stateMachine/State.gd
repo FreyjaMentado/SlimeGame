bec5de12c538542d804e0c577bb8d0e1a5685cb6
Freyja Shelton <freyjamentado@gmail.com>
2024-01-14 17:21:32 -0500

    update comment

--- player/stateMachine/State.gd
+++ player/stateMachine/State.gd
@@ -3,7 +3,7 @@ extends Node
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Hold a reference to the parent so that it can be controlled by the state
+# Hold a reference to the player so that it can be controlled by the state
 var player: CharacterBody2D
 
 func enter() -> void:
