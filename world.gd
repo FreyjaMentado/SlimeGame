a838caaf741a9f8eadf8ebc551878687735a1e02
Freyja Shelton <freyjamentado@gmail.com>
2024-01-20 12:51:22 -0500

    add base menus for start, options and score screen

--- world.gd
+++ world.gd
@@ -6,9 +6,6 @@ extends Node2D
 @export var slime_trail_left : PackedScene
 @export var slime_trail_right : PackedScene
 
-var slime_left
-var slime_right
-var slime_bottom
 var slime_left_area
 var slime_right_area
 var slime_bottom_left_area
@@ -21,9 +18,6 @@ enum side {
 }
 
 func _ready():
-	slime_left = player.get_node("LeftSlime")
-	slime_right = player.get_node("RightSlime")
-	slime_bottom = player.get_node("BottomSlime")
 	slime_left_area = player.get_node("LeftSlimeArea")
 	slime_right_area = player.get_node("RightSlimeArea")
 	slime_bottom_left_area = player.get_node("BottomLeftSlimeArea")
@@ -33,21 +27,17 @@ func _physics_process(delta):
 	handle_slime_trail()
 
 func handle_slime_trail():
-	var collider_object : Object
+	if player.is_on_wall():
+		var wall_normal = player.get_wall_normal()
+		if wall_normal.x < 0:
+			if !slime_left_area.is_colliding() and !slime_bottom_left_area.is_colliding():
+				handle_spawn_slime(side.left)
+		if wall_normal.x > 0:
+			if !slime_right_area.is_colliding() and !slime_bottom_right_area.is_colliding():
+				handle_spawn_slime(side.right)
 	
-	if slime_left.is_colliding():
-		if !slime_left_area.is_colliding() and !slime_bottom_left_area.is_colliding():
-			print("spawn")
-			handle_spawn_slime(side.left)
-	
-	if slime_right.is_colliding():
-		if !slime_right_area.is_colliding() and !slime_bottom_right_area.is_colliding():
-			print("spawn")
-			handle_spawn_slime(side.right)
-	
-	if slime_bottom.is_colliding():
+	if player.is_on_floor():
 		if !slime_bottom_left_area.is_colliding() and !slime_bottom_right_area.is_colliding():
-			print("spawn")
 			handle_spawn_slime(side.bottom)
 		
 
@@ -62,3 +52,9 @@ func handle_spawn_slime(side_to_spawn: side):
 			slime = slime_trail.instantiate()
 	slime.transform = player.global_transform
 	add_child(slime)
+
+
+func _on_end_game_timeout():
+	print("Game over!")
+	print("Score: ", get_tree().get_nodes_in_group("Slime").size())
+	get_tree().change_scene_to_file("res://score_menu.tscn")
