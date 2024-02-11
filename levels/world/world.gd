52183ee908531de92ef2b60234da668bc5d6ceaa
Freyja Shelton <freyjamentado@gmail.com>
2024-02-11 10:04:17 -0500

    implement basic scene switcher

--- levels/world/world.gd
+++ levels/world/world.gd
@@ -4,14 +4,15 @@ func _ready():
 	$Ground/TileMap/StartMessage.show()
 	$Ground/TileMap/MessageTimer.start()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var formattedTime = floatToMinutesSeconds($EndGame.time_left)
 	$CanvasLayer/Timer.text = formattedTime
 
 func _on_end_game_timeout():
-	var game_vars = get_node("/root/GameVariables")
-	game_vars.score = get_tree().get_nodes_in_group("Slime").size()
-	get_tree().change_scene_to_file("res://screens/endScreen/score_menu.tscn")
+	end_game()
+
+func _on_area_2d_body_entered(_body):
+	end_game()
 
 func _on_message_timer_timeout():
 	$Ground/TileMap/StartMessage.hide()
@@ -25,3 +26,10 @@ func floatToMinutesSeconds(timeInSeconds: float) -> String:
 	var secondsStr = str(seconds).pad_zeros(2)
 	
 	return minutesStr + ":" + secondsStr
+
+func end_game():
+	var game_vars = get_node("/root/GameVariables")
+	game_vars.score = get_tree().get_nodes_in_group("Slime").size()
+	SceneSwitcher.switch_scene("res://screens/endScreen/score_menu.tscn")
+
+
