904c9621c34f4ca595cf165ba56572ab40fd3a75
Freyja Shelton <freyjamentado@gmail.com>
2024-05-10 13:54:07 -0400

    fix timers and lock sprite

--- levels/world/world.gd
+++ levels/world/world.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
-func _on_area_2d_body_entered(_body):
-	var game_vars = get_node("/root/GameVariables")
-	game_vars.score = get_tree().get_nodes_in_group("Slime").size()
+@onready var start_message = $Ground/TileMap/StartMessage
+@onready var score_timer = $Timers/ScoreTimer
+@onready var minutes_label = $CanvasLayer/Minutes
+@onready var seconds_label = $CanvasLayer/Seconds
+
+func _physics_process(delta):
+	var minutes = int(score_timer.time_left) / 60
+	var seconds = int(score_timer.time_left) % 60
+	minutes_label.text = str(minutes)
+	seconds_label.text = str(seconds)
+
+func _on_score_timer_timeout():
 	SceneSwitcher.switch_scene("res://screens/endScreen/score_menu.tscn")
+
+func _on_message_timer_timeout():
+	start_message.visible = false
